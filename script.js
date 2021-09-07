fc5a9b2c1ec7076a20fc9187275d2a16de1b6165
Matheus Santos <87808357+matheussantosspbr@users.noreply.github.com>
2021-09-07 20:24:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var nome = window.prompt('Qual é seu nome ?')
+window.alert('Ok ' + nome + ', Vamos testar essa calculadora de adição !!')
+window.alert('Obs : ' + nome + ' vai ser somente conta de mais !!')
+var n1 = Number(window.prompt('Digite um Número !'))
+var n2 = Number(window.prompt('Digite outro Número !'))
+var s = n1 + n2
+window.alert(nome + ', a Soma desses dois numeros é ' + s + ' Certo ?')
